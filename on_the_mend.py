b039e6de9bb711d058d1ca75fac029e20bd274c8
Davis Hunter <108430642+kingarthurdavis78@users.noreply.github.com>
2022-09-21 22:10:42 -0600

    update single player

--- on_the_mend.py
+++ on_the_mend.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-from game_logic import Bob, screen, screen_width, screen_height, new_bullet, Gun, generate_new_zombie, zombie_timer, zombie_frequency, Crosshair
+from game_logic import Bob, screen, screen_width, screen_height, unit_length, new_bullet, Gun, generate_new_zombie, zombie_timer, zombie_frequency, Crosshair
 
 pygame.init()
 
@@ -10,8 +10,8 @@ pygame.mouse.set_visible(False)
 mainLoop = True
 background = 150, 150, 150
 
-gun = Gun("pistol", 1, 600)
-bob = Bob(0, "left", 0.3, gun)
+gun = Gun("pistol", 1, 100)
+bob = bob = Bob(screen_width / 2, screen_height /2, 0 ,"left", 0.3, gun, Crosshair(Crosshair.image))
 zombies = [generate_new_zombie(0.14)]
 bullets = []
 crosshair = Crosshair(Crosshair.image)
@@ -26,6 +26,32 @@ while mainLoop:
     dt = clock.tick(60)
 
     screen.fill(background)
+
+
+    # Zombies
+    for zombie in zombies:
+        if bob.rect.colliderect(zombie.rect):
+            died = True
+        zombie.rect = zombie.rect.move([t * dt for t in zombie.get_speed(bob.rect)])
+        zombie.count += 1
+        if zombie.count > 15:
+            zombie.get_step()
+        zombie.paint(bob.rect)
+
+
+
+    # Bob
+    bob.get_velocity(dt)
+    bob.get_direction()
+    if bob.count > 200:
+        bob.get_step()
+    bob.rect = bob.rect.move([v * dt for v in bob.velocity])
+    bob.paint()
+
+    # Crosshair
+    bob.update_crosshair(0, 0)
+    # bob.cross_dx, bob.cross_dy = bob.update_crosshair(bob.cross_dx, bob.cross_dy)
+    bob.crosshair.paint()
 
     # Bullets
     bob.gun.reload_counter += dt
@@ -55,32 +81,10 @@ while mainLoop:
             bullet.rect = bullet.rect.move([v * dt * bullet.speed for v in bullet.velocity])
             bullet.paint()
 
-
-    # Bob
-    bob.get_velocity(dt)
-    bob.get_direction()
-    if bob.count > 200:
-        bob.get_step()
-    bob.rect = bob.rect.move([v * dt for v in bob.velocity])
-    bob.paint()
-
-    # Zombies
-    for zombie in zombies:
-        if bob.rect.colliderect(zombie.rect):
-            died = True
-        zombie.rect = zombie.rect.move([t * dt for t in zombie.get_speed(bob.rect)])
-        zombie.count += 1
-        if zombie.count > 15:
-            zombie.get_step()
-        zombie.paint(bob.rect)
-
     if pygame.time.get_ticks() > zombie_timer:
         zombie_timer += zombie_frequency
         zombies.append(generate_new_zombie(0.14))
 
-    # Crosshair
-    crosshair.rect.center = pygame.mouse.get_pos()  # update position
-    screen.blit(crosshair.image, crosshair.rect)  # draw the cursor
 
     pygame.display.update()
     if pygame.key.get_pressed()[K_ESCAPE] or died:
