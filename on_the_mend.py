b48950aae452300c4996701619a7b416a93fea03
Davis Hunter <108430642+kingarthurdavis78@users.noreply.github.com>
2022-09-25 15:58:38 -0600

    Add bob health

--- on_the_mend.py
+++ on_the_mend.py
@@ -18,6 +18,7 @@ mainLoop = True
 background = 150, 150, 150
 
 num_players = int(sys.argv[1])
+players_alive = num_players
 
 
 keyboard_controls = False
@@ -33,6 +34,7 @@ for i in range(num_players):
     crosshairs.append(Crosshair(i, colors[i]))
 
 bobs = []
+dead_bobs = []
 
 if keyboard_controls:
     bobs.append(Bob("keys", screen_width / 2, screen_height / 2, 0, "left", 0.3, guns.pop(0), crosshairs.pop(0)))
@@ -58,9 +60,9 @@ while mainLoop:
                     bt += 1
             usb = controller_count - bt
             for i in range(bt):
-                bobs.append(Bob_Joystick_Bluetooth(i, screen_width / 2, screen_height / 2, 0, "left", 0.3, guns[i], joysticks[i], crosshairs[i]))
+                bobs.append(Bob_Joystick_Bluetooth(f"Wireless Controller {i}", screen_width / 2, screen_height / 2, 0, "left", 0.3, guns[i], joysticks[i], crosshairs[i]))
             for i in range(usb):
-                bobs.append(Bob_Joystick_USB(i, screen_width / 2, screen_height / 2, 0, "left", 0.3, guns[i], joysticks[i], crosshairs[i]))
+                bobs.append(Bob_Joystick_USB(f"USB Controller {i}", screen_width / 2, screen_height / 2, 0, "left", 0.3, guns[i], joysticks[i], crosshairs[i]))
         except IndexError:
             pass
         if len(bobs) == num_players:
@@ -86,39 +88,68 @@ while mainLoop:
         zombie.paint(bobs[0].rect)
 
     # Generate Zombie
-    if random.randint(1, 100) > 95:
+    if random.randint(0, 100) < 5:
         zombies.append(generate_new_zombie(0.1))
 
-    # Generate Zombie
-    if random.randint(1, 1000) > 995:
+    # # Generate Zombie
+    if random.randint(0, 1000) < 5:
         zombies.append(generate_new_zombie(0.3))
 
     # Bobs
     for bob in bobs:
-        # Check if bob died
-        for zombie in zombies:
-            if bob.rect.colliderect(zombie.rect):
-                died = True
+        if bob.is_alive:
+            # Check if bob loses health
+            for zombie in zombies:
+                if bob.rect.colliderect(zombie.rect):
+                    bob.health -= 1
 
-        # Update Bob
-        bob.get_velocity(dt)
-        bob.get_direction()
-        if bob.count > 200:
-            bob.get_step()
-        bob.rect = bob.rect.move([v * dt for v in bob.velocity])
+            # Paint healthbar
+            player_number = bobs.index(bob)
+            bob.paint_health(player_number, num_players)
 
-        # Paint bob
-        bob.paint()
-        # Bobs' Bullets
-        bob.gun.reload_counter += dt
-        if bob.shoot() and bob.gun.reload_counter > bob.gun.reload_time:
-            bob.gun.reload_counter = 0
-            bullets.append(new_bullet(bob, bob.gun.speed))
-        bullets, zombies = paint_bullets(bullets, zombies, dt)
+            # Update Bob
+            bob.get_velocity(dt)
+            bob.get_direction()
+            if bob.count > 200:
+                bob.get_step()
+            bob.rect = bob.rect.move([v * dt for v in bob.velocity])
 
-        # Bob's Crosshair
-        bob.cross_dx, bob.cross_dy = bob.update_crosshair(bob.cross_dx, bob.cross_dy)
-        bob.crosshair.paint()
+            # Paint bob
+            bob.paint()
+
+            # Bobs' Bullets
+            bob.gun.speed = 1 / players_alive
+            bob.gun.reload_counter += dt
+            if bob.shoot() and bob.gun.reload_counter > bob.gun.reload_time:
+                bob.gun.reload_counter = 0
+                bullets.append(new_bullet(bob, bob.gun.speed))
+            bullets, zombies = paint_bullets(bullets, zombies, dt)
+
+            # Bob's Crosshair
+            bob.cross_dx, bob.cross_dy = bob.update_crosshair(bob.cross_dx, bob.cross_dy)
+            bob.crosshair.paint()
+
+            # Revive Teamate
+            # if num_players - players_alive > 0:
+            #     for dead_bob in dead_bobs:
+            #         if bob.revive():
+            #             revive_count += dt
+            #             if revive_count > revive_time:
+            #
+            #         else:
+            #             revive_count = 0
+
+            # Check if died
+            if bob.health <= 0:
+                bob.is_alive = False
+                players_alive -= 1
+                dead_bobs.append(bob)
+
+        else:
+            bob.paint()
+
+    if players_alive == 0:
+        died = True
 
     pygame.display.update()
 
